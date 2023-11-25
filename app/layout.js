f8bc92be58d853b75c33557536ec6fa8be3b2bf2
Stefano Galiffa <paninos79@yahoo.it>
2023-11-25 19:38:22 +0000

    optimize font

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className="font-poppins text-2xl xl:text-3xl">
+			<body className="font-poppins font-rendering text-2xl xl:text-3xl">
 				{children}
 				<Footer />
 			</body>
