b2018c707ee79582a3ce3300641c0152064b5e59
Stefano Galiffa <paninos79@yahoo.it>
2025-09-23 16:37:54 +0200

    Trigger rebuild

--- app/components/IntroTourDates/IntroTourDates.js
+++ app/components/IntroTourDates/IntroTourDates.js
@@ -72,6 +72,7 @@ export default function IntroTourDates() {
 							<div className="hidden text-right text-white leading-none md:flex">
 								<MapMapper extraClasses="fill-amber-400 mr-2" />
 								<p>{gig.city}</p>
+								{/*  */}
 							</div>
 						</li>
 					);
